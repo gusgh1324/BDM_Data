3e734e92ae6fa95aeed8de23c920b64539f267d5
HwangJanghyeon <150228905+HwangJanghyeon@users.noreply.github.com>
2024-05-08 14:58:37 +0900

    Update img3.py-2024.05.08_1458

--- 이미지 학습 코드/img3.py
+++ 이미지 학습 코드/img3.py
@@ -1,56 +1,73 @@
-import torch
-import torch.nn as nn
-import torch.optim as optim
-import torchvision
-import torchvision.transforms as transforms
-from torch.utils.data import DataLoader, Dataset
+import numpy as np
+import matplotlib.pyplot as plt
 
-# 샴 네트워크 모델 정의
-class ShallowNet(nn.Module):
-    def __init__(self, embedding_size):
-        super(ShallowNet, self).__init__()
-        self.conv1 = nn.Conv2d(3, 32, kernel_size=3, padding=1)
-        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
-        self.pool = nn.MaxPool2d(2, 2)
-        self.fc1 = nn.Linear(64 * 8 * 8, embedding_size)
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.applications.mobilenet_v2 import MobileNetV2
+from tensorflow.keras import Sequential
+from tensorflow.keras.layers import Dense, Dropout, GlobalAveragePooling2D
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.callbacks import ReduceLROnPlateau, EarlyStopping, ModelCheckpoint
 
-    def forward(self, x):
-        x = self.pool(torch.relu(self.conv1(x)))
-        x = self.pool(torch.relu(self.conv2(x)))
-        x = torch.flatten(x, 1)
-        x = self.fc1(x)
-        return x
+train_datagen = ImageDataGenerator(
+        rescale=1./255,
+        rotation_range=30,
+        shear_range=0.3,
+        horizontal_flip=True,
+        width_shift_range=0.1,
+        height_shift_range=0.1,
+        zoom_range=0.25,
+)
+valid_datagen = ImageDataGenerator(
+        rescale=1./255,
+)
 
-# CIFAR-10 데이터셋 불러오기
-transform = transforms.Compose([
-    transforms.ToTensor(),
-    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
-])
+batch_size = 64
+img_width = 128
+img_height = 128
 
-trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
-trainloader = DataLoader(trainset, batch_size=32, shuffle=True)
+train_data = train_datagen.flow_from_directory(
+    './train_dataset/train',
+    batch_size=batch_size,
+    target_size=(img_width, img_height),
+    shuffle=True,
+)
+valid_data = valid_datagen.flow_from_directory(
+    './train_dataset/validation',
+    target_size=(img_width, img_height),
+    batch_size=batch_size,
+    shuffle=False,
+)
 
-# 모델 생성 및 손실 함수, 최적화 기법 정의
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-net = ShallowNet(embedding_size=64).to(device)
-criterion = nn.CrossEntropyLoss()
-optimizer = optim.Adam(net.parameters(), lr=0.001)
+def visualize_images(images, labels):
+    figure, ax = plt.subplots(nrows=3, ncols=3, figsize=(12, 14))
+    classes = list(train_data.class_indices.keys())
+    img_no = 0
+    for i in range(3):
+        for j in range(3):
+            img = images[img_no]
+            label_no = np.argmax(labels[img_no])
 
-# 모델 학습
-for epoch in range(5):  # 데이터셋을 여러번 반복
-    running_loss = 0.0
-    for i, data in enumerate(trainloader, 0):
-        inputs, labels = data[0].to(device), data[1].to(device)
-        optimizer.zero_grad()
-        outputs = net(inputs)
-        loss = criterion(outputs, labels)
-        loss.backward()
-        optimizer.step()
+            ax[i,j].imshow(img)
+            ax[i,j].set_title(classes[label_no])
+            ax[i,j].set_axis_off()
+            img_no += 1
 
-        running_loss += loss.item()
-        if i % 100 == 99:    # 매 100 미니배치마다 출력
-            print('[%d, %5d] loss: %.3f' %
-                  (epoch + 1, i + 1, running_loss / 100))
-            running_loss = 0.0
+images, labels = next(train_data)
+visualize_images(images, labels)
 
-print('Finished Training')
+base = MobileNetV2(input_shape=(img_width, img_height,3),include_top=False,weights='imagenet')
+base.trainable = True
+model = Sequential()
+model.add(base)
+model.add(GlobalAveragePooling2D())
+model.add(Dense(128, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(2, activation='softmax'))
+opt = Adam(learning_rate=0.001)
+model.compile(optimizer=opt,loss = 'categorical_crossentropy',metrics=['accuracy'])
+
+reduce_lr = ReduceLROnPlateau(monitor = 'val_accuracy',patience = 1,verbose = 1)
+early_stop = EarlyStopping(monitor = 'val_accuracy',patience = 5,verbose = 1,restore_best_weights = True)
+check_point = ModelCheckpoint(filepath='fish_class_cnn.keras', monitor='val_accuracy', verbose=1, save_best_only=True)
+
+history = model.fit(train_data, epochs=50, validation_data = valid_data, callbacks=[early_stop,reduce_lr,check_point])

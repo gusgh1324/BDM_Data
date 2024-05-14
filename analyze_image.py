676238734b4d85fa4d7df5cd6cf6a8fd30c4cd33
HwangJanghyeon <150228905+HwangJanghyeon@users.noreply.github.com>
2024-05-14 16:24:22 +0900

    Update analyze_image.py-20240514_1624
    분석 결과를 JSON 형태로 전환시키는 기능 추가

--- analyze_image.py
+++ analyze_image.py
@@ -2,20 +2,22 @@ import sys
 import tensorflow as tf
 from tensorflow.keras.preprocessing import image
 import numpy as np
+import json
 
-image_path='train_dataset/test/아가미흡충/아가미흡충 농어 2.jpg'
+image_path = 'train_dataset/test/아가미흡충/아가미흡충 농어 2.jpg'
+
+
 # 이미지를 분석하는 함수
 def analyze_image(image_path):
     # 각 모델의 경로 설정
     model_paths = ['fish_class_cnn.keras', 'fish_class_tl.keras', 'fish_class_dnn.keras']
     # 결과를 저장할 리스트
     results = []
-
+    print("분석 시작")
     # 각 모델에 대해 반복
     for idx, model_path in enumerate(model_paths):
         # TensorFlow 모델 로드
         model = tf.keras.models.load_model(model_path)
-
         # 이미지 불러오기 및 전처리
         img = image.load_img(image_path, target_size=(150, 150))  # 이미지 크기를 모델에 맞게 조정
         img_array = image.img_to_array(img)
@@ -29,13 +31,10 @@ def analyze_image(image_path):
         classes = ['림포시스티스병', '비브리오', '아가미흡충', '연쇄구균병']  # 클래스 레이블을 적절하게 수정
 
         # 클래스 별 확률 가져오기
-        class_probabilities = {class_name: probability for class_name, probability in zip(classes, prediction[0])}
-
-        # 확률이 높은 순으로 정렬된 딕셔너리 생성
-        sorted_probabilities = dict(sorted(class_probabilities.items(), key=lambda item: item[1], reverse=True))
+        class_probabilities = {class_name: float(probability) for class_name, probability in zip(classes, prediction[0])}
 
         # 결과 튜플로 저장하여 리스트에 추가
-        result = (f"{['CNN', '전이학습', 'DNN'][idx]} 모델", sorted_probabilities)
+        result = (f"{['CNN', '전이학습', 'DNN'][idx]} 모델", class_probabilities)
         results.append(result)
 
         # 현재까지의 모델 분석 상태 출력
@@ -47,4 +46,7 @@ def analyze_image(image_path):
 if __name__ == "__main__":
     # 이미지 분석 결과 출력
     result = analyze_image(image_path)
+    # 결과를 Python 리스트로 변환하여 JSON으로 직렬화
+    json_result = json.dumps(result, default=lambda o: o.__dict__, indent=4)
+    print(json_result)
     print(result)
